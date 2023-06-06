6b5cb9be0a423683f0f32179ed209c96d1f07291
Dharu65 <98dharu.prasetyo@gmail.com>
2023-06-06 15:02:37 +0700

    filtering test

---
+++ filteringtest/filteringtest01.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// nomor 2
+func segitiga1() {
+	rows := 7
+	start := 1
+	end := 13
+	count := start
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j <= i; j++ {
+			if count > end {
+				count = start
+			}
+			fmt.Printf("%-3d", count)
+			count++
+		}
+		fmt.Println()
+		count = start + i + 1
+	}
+}
+
+func segitga2() {
+	rows := 7
+	start := 5
+	end := 17
+	count := start
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j <= i; j++ {
+			if count > end {
+				count = start
+			}
+			fmt.Printf("%-3d", count)
+			count++
+		}
+		fmt.Println()
+		count = start + i + 1
+	}
+}
+
+// nomor 3
+func findTarget(nums []int, target int) int {
+	for i, num := range nums {
+		if num == target {
+			return i
+		}
+	}
+	return -1 // Jika target tidak ditemukan
+}
+
+// nomor 4
+func addSum(nums []int, target int) []int {
+	indexs := make([]int, 0)
+
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				indexs = append(indexs, i, j)
+			}
+		}
+	}
+	return indexs
+}
+
+// nomor 5
+func sumZero(nums []int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			for k := j + 1; k < len(nums); k++ {
+				if nums[i]+nums[j]+nums[k] == 0 {
+					return []int{nums[i], nums[j], nums[k]}
+				}
+			}
+		}
+	}
+
+	return []int{} // Jika tidak ditemukan pasangan elemen
+}
+
+// nomor 6
+func plusOneDigit(digits []int) []int {
+	n := len(digits)
+	carry := 1
+
+	for i := n - 1; i >= 0; i-- {
+		sum := digits[i] + carry
+		digits[i] = sum % 10
+		carry = sum / 10
+	}
+
+	if carry > 0 {
+		digits = append([]int{carry}, digits...)
+	}
+
+	return digits
+}
+
+// nomor 7
+func compareSlices(slice1 []string, slice2 []string) ([]string, []string) {
+	var same []string
+	var different []string
+
+	for _, item1 := range slice1 {
+		found := false
+
+		for _, item2 := range slice2 {
+			if item1 == item2 {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			same = append(same, item1)
+		} else {
+			different = append(different, item1)
+		}
+	}
+
+	for _, item2 := range slice2 {
+		found := false
+
+		for _, item1 := range slice1 {
+			if item2 == item1 {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			different = append(different, item2)
+		}
+	}
+
+	return same, different
+}
+
+// nomor 8
+func isPalindrome(words []string) bool {
+	length := len(words)
+	for i := 0; i < length/2; i++ {
+		if words[i] != words[length-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+// nomor 9
+func minMaxArray(arr []int) (int, int) {
+	min := arr[0]
+	max := arr[0]
+
+	for _, num := range arr {
+		if num < min {
+			min = num
+		}
+		if num > max {
+			max = num
+		}
+	}
+	return min, max
+}
+
+// nomor 10
+func menyisipkan(arr []int, target int) []int {
+	// Menambahkan elemen target ke dalam array
+	arr = append(arr, target)
+
+	// Mengurutkan array secara ascending
+	sort.Ints(arr)
+
+	return arr
+}
+
+func main() {
+	// no.2
+	//segitiga1()
+	//segitga2()
+
+	// no.3
+	//result := findTarget([]int{1, 2, 4}, 4)
+	//result := findTarget([]int{-1, 2, 5, 6, 7}, 6)
+	//fmt.Println(result)
+
+	// no.4
+	//fmt.Println(addSum([]int{3, 2, 3}, 6))
+	//fmt.Println(addSum([]int{3, 2, 4}, 6))
+	//fmt.Println(addSum([]int{3, 3}, 6))
+
+	// no.5
+	result := sumZero([]int{-1, 0, 1, 2, -1, -4})
+	fmt.Println(result)
+
+	// no.6
+	//digits := []int{1, 2, 3}
+	//fmt.Println(plusOneDigit(digits))
+
+	// no.7
+	//slice1 := []string{"mangga", "apel", "melon", "pisang", "sirsak", "tomat", "nanas", "nangka", "timun", "mangga"}
+	//slice2 := []string{"bayam", "wortel", "kangkung", "mangga", "tomat", "kembang kol", "nangka", "timun"}
+	//same, different := compareSlices(slice1, slice2)
+	//fmt.Printf("Same: %v\n", same)
+	//fmt.Printf("Different: %v\n", different)
+
+	// no.8
+	//fmt.Println(isPalindrome([]string{"asep", "budi", "-", "budi", "asep"}))
+	//fmt.Println(isPalindrome([]string{"tom", "tim", "tim", "tom"}))
+	//fmt.Println(isPalindrome([]string{"tik", "tok", "toko", "tik"}))
+
+	// no.9
+	//fmt.Println(minMaxArray([]int{2, 3, 4, 5, 6, 7, 8, 9, 1, 10}))
+
+	// no.10
+	//array := []int{4, 7, 1, 20}
+	//target := 9
+	//result := menyisipkan(array, target)
+	//fmt.Println(result)
+
+}
---
+++ filteringtest/nomor 1filtertest.sql
@@ -0,0 +1,81 @@
+create table t_policy (
+	policy_number integer,
+	policy_submit_date date,
+	premium money,
+	discount integer,
+	commission money,
+	client_number varchar(20) unique,
+	agent_code varchar(20) unique,
+	policy_status varchar(25),
+	policy_due_date date,
+	constraint t_policy_pk primary key (policy_number, client_number, agent_code)
+)
+
+create table t_agent(
+	agent_code varchar(20),
+	agent_name varchar(50),
+	agent_office varchar(30),
+	basic_commission money,
+	foreign key (agent_code) references t_policy(agent_code)
+)
+
+create table t_client(
+	client_number varchar(20),
+	client_name varchar(50),
+	birth_date date,
+	foreign key (client_number) references t_policy(client_number)
+)
+
+select * from t_policy
+
+insert into t_policy (policy_number, policy_submit_date, premium, discount, commission, client_number, agent_code, policy_status, policy_due_date) values 
+(001, '2018-1-5', 18600000, 10, 930000, 'CL001', 'AG001', 'INFORCE', null),
+(002, '2018-1-5', 2500000, 10, 125000, 'CL002', 'AG002', 'INFORCE', null),
+(003, '2018-1-10', 2500000, 10, 125000, 'CL003', 'AG003', 'TERMINATE', null),
+(004, '2018-1-25', 4000000, 10, 200000, 'CL004', 'AG004', 'PROPOSAL', null),
+(005, '2018-1-25', 2625000, 10, 131250, 'CL005', 'AG005', 'PROPOSAL', null)
+
+insert into t_agent (agent_code, agent_name, agent_office, basic_commission) values
+('AG001', 'LIDYA', 'JAKARTA', NULL),
+('AG002', 'RUDI', 'BALI', NULL),
+('AG003', 'DICKY', 'BALI', NULL),
+('AG004', 'DAVID', 'SURABAYA', NULL),
+('AG005', 'FARIZ', 'SURABAYA', NULL)
+
+insert into t_client (client_number, client_name, birth_date) values
+('CL001', 'WAYNE ROONEY', '1956-5-11'),
+('CL002', 'RYAN GIGGS', '1972-9-3'),
+('CL003', 'DAVID BECKHAM', '1985-9-3'),
+('CL004', 'MICHAEL OWEN', '2012-9-3')
+
+select * from t_policy a
+	left join t_client b
+	on a.client_number = b.client_number
+	where policy_submit_date > '2018-01-15' and extract(month from birth_date) = 9;
+	
+SELECT * FROM t_policy a
+	LEFT JOIN t_agent b
+	ON a.agent_code = b.agent_code
+	WHERE policy_status = 'INFORCE' AND agent_office = 'JAKARTA';
+	
+SELECT a.agent_code, (b.commission/b.premium * 100) AS basic_commission
+    FROM t_agent a
+    JOIN t_policy b ON a.agent_code = b.agent_code;	
+	
+UPDATE t_policy
+SET policy_due_date = DATE_TRUNC('MONTH', policy_submit_date + INTERVAL '30 days') + INTERVAL '1 MONTH' - INTERVAL '1 DAY';
+	
+SELECT a.agent_code, a.agent_name, a.agent_office, p.premium - cast(p.premium * 0.1 AS MONEY) AS produksi_agent
+    FROM t_agent AS a 
+    LEFT JOIN t_policy as p ON p.agent_code = a.agent_code
+    WHERE cast(p.premium * 0.1 AS MONEY) < cast(1000000 AS MONEY)
+    ORDER BY produksi_agent ASC	
+	
+	
+	
+	
+	
+	
+	
+	
+	
